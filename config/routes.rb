2af5bed64e6ba072b2a4a0cd170988e135ed252e
Juan Pablo Portillo <juanp.portillob@gmail.com>
2023-09-20 11:02:52 -0600

    added a new gallery page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post "contact-email", to: "pages#contact_email", as: "contact_email"
   resources :contact_submissions, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/portfolio', to: 'portfolio#index'
 
   # Defines the root path route ("/")
   # root "articles#index"
