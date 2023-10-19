398d60c3e43870253135ba6517d70b75dc203676
Juan Pablo Portillo <juanp.portillob@gmail.com>
2023-10-19 10:50:43 -0600

    received email updated

--- app/mailers/client_mailer.rb
+++ app/mailers/client_mailer.rb
@@ -1,9 +1,9 @@
 class ClientMailer < ApplicationMailer
   def send_client_email
     @contact_submission = params[:contact_submission]
-
+    to = Rails.env.production? ? ENV["RECEIVER_EMAIL"] : Rails.application.credentials[:sendgrid][:receiver_email]
     mail(
-      to: "alienordarenberg@gmail.com",
+      to: ENV["RECEIVER_EMAIL"],
       subject: "New Wedding Contact Form Submission"
     )
   end
